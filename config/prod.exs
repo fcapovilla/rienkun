89c0a71dc6b0d690d61d5db2e9ddfe90f6b37d1d
Frédérick Capovilla <caposite@hotmail.com>
2021-02-06 16:51:38 -0500

    Autre config pour gigalixir.

--- config/prod.exs
+++ config/prod.exs
@@ -10,10 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :rienkun, RienkunWeb.Endpoint,
-  load_from_system_env: true,
   server: true,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
